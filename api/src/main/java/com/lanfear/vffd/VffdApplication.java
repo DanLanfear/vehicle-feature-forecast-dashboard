5a360cfd41fcad86a8437e7c3c34abfe928ec457
Dan Lanfear <lanfeadb@udmercy.edu>
2026-05-15 18:58:32 -0400

    Added spring app from springinitializr

---
+++ api/src/main/java/com/lanfear/vffd/VffdApplication.java
@@ -0,0 +1,13 @@
+package com.lanfear.vffd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VffdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VffdApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/lanfear/vffd/VffdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanfear.vffd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VffdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
