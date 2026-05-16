d10d6b5f6f5529a780efaee072b524cc6fa79ec8
DanLanfear <lanfeadb@udmercy.edu>
2026-05-16 17:16:03 -0400

    created ForecastSummaryDTO

--- vffd-api/src/main/java/com/lanfear/vffd/ForecastSummaryDTO.java
+++ vffd-api/src/main/java/com/lanfear/vffd/ForecastSummaryDTO.java
@@ -1,4 +1,16 @@
 package com.lanfear.vffd;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ForecastSummaryDTO {
+    private List<Long> forecastRecordIds;
+    private Integer totalProjectedVolume;
+    private Double averageAdoptionRate;
 }
