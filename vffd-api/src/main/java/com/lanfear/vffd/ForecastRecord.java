fbda9c431753f4fe5a4abbbe9c258ee932af2ca4
Dan Lanfear <lanfeadb@udmercy.edu>
2026-05-16 08:37:19 -0400

    Created forecast record model

---
+++ vffd-api/src/main/java/com/lanfear/vffd/ForecastRecord.java
@@ -0,0 +1,32 @@
+package com.lanfear.vffd;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ForecastRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String vehicleModel;
+    private Integer modelYear;
+    private String region;
+    private String quarter;
+    private String featureCode;
+    private String featureName;
+    private Integer projectedVolume;
+    private Float adoptionRate;
+    private String lastUpdatedBy;
+    
+}
