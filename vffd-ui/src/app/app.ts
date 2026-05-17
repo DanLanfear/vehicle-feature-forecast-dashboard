27a464b86524adefec73729c6dfe7654f796dbf3
DanLanfear <lanfeadb@udmercy.edu>
2026-05-17 00:18:28 -0400

    updated app component to display dashboard component

--- vffd-ui/src/app/app.ts
+++ vffd-ui/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('vffd-ui');
+  protected readonly title = 'Vehicle Feature Forecast Dashboard';
 }
